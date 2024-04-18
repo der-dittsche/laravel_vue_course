f8b023fc4206a7fe76dfba403640ffe7102c8360
derdittsche <sascha_dietrich@outlook.com>
2024-04-18 17:52:38 +0200

    feat: add sanctum authentication by using spa and api tokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -44,5 +43,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
     }
 }
